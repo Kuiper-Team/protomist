636bc85e064413ca8bd5c97c9037888625123533
Kuineer <nihat.ozkurt@yaani.com>
2026-07-09 14:36:39 +0300

    Bech32 wrapper is (hopefully) ready.

--- source/bech32/wrapper.cpp
+++ source/bech32/wrapper.cpp
@@ -7,16 +7,35 @@
 #include "../result.h"
 
 result MIST_BECH32M_ENCODE(
-    unsigned char* output
+    unsigned char* output,
+
+    const unsigned char* MIST_DECODED,
+    const size_t decoded_size
 ) {
-    const std::vector<uint8_t> data; //How?
-    Encode(bech32m_encoding, bech32m_hrp_1, data); //Error handling?
+    std::vector<uint8_t> data(MIST_DECODED, MIST_DECODED + decoded_size); //Converting array to vector
+
+    const char* encoded = Encode(Encoding::BECH32M, bech32m_hrp_1, data).c_str(); //Error handling?
+    strcpy(output, encoded);
 
     return success;
 }
 
 result MIST_BECH32M_DECODE(
-    unsigned char* output
+    unsigned char* output,
+
+    unsigned char* MIST_ENCODED
 ) {
+    struct DecodeResult decoded = Decode(MIST_ENCODED);
+    if (decoded.empty())
+        return bech32_decoding_error;
+
+    uint8_t* output_uint8_t;
+    memcpy(output_uint8_t, &decoded.data[0]);
+
+    size_t output_size = decoded.data.size();
+
+    for (int index = 0; index < output_size; index++)
+        output[index] = (unsigned char) output_uint8_t[index];
+
     return success;
 }
